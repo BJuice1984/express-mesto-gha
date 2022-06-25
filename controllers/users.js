08986647d2983a8e0ca312dd404ad6296726e762
BJuice <beetle_juice84@mail.ru>
2022-06-26 02:47:34 +0300

    feat/создать контроллер и роутер Users

---
+++ controllers/users.js
@@ -0,0 +1,8 @@
+const User = require('../models/user');
+
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send(users))
+    .catch(() => res.status(500)
+      .send({ message: 'Ошибка Users' }));
+};
---
+++ models/card.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlenght: 2,
+    maxlenght: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owener: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  likes: {
+    type: [mongoose.Schema.Types.ObjectId],
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
