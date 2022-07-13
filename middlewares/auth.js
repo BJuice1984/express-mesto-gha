22a588d4b758071abd9a8288958cc433658752c2
BJuice <beetle_juice84@mail.ru>
2022-07-14 01:10:59 +0300

    fix/исправить проверку токена

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,26 +1,40 @@
 const { checkToken } = require('../helpers/jwt');
-const UnauthorizationError = require('../errors/unauth-err');
+const User = require('../models/user');
+
+const throwUnauthorizedError = () => {
+  const error = new Error('Авторизуйтесь для доступа');
+  error.statusCode = 401;
+  throw error;
+};
 
 module.exports = (req, res, next) => {
-  console.log('In auth');
   const auth = req.cookies.jwt;
 
   console.log('auth', auth);
 
+  if (!auth) {
+    throwUnauthorizedError();
+  }
+
   const token = auth.replace('Bearer ', '');
 
-  if (!token) {
-    throw new UnauthorizationError('Ошибка. Необходима авторизация');
-  }
   let payload;
   try {
     payload = checkToken(token);
+    console.log('payload', payload);
+
+    User.findById(payload._id)
+      .then((user) => {
+        console.log('user', user);
+        if (!user) {
+          throwUnauthorizedError();
+        }
+      });
   } catch (err) {
     res.clearCookie('token');
-    throw new UnauthorizationError('Ошибка. Необходима авторизация');
+    throwUnauthorizedError();
   }
   req.user = payload;
-  console.log('auth', req.user);
 
   next();
 };
